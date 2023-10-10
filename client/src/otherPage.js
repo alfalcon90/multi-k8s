e99546eaf056d19c0005f7eb3c4c4adb98088095
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2023-10-10 16:20:45 -0700

    initial commit

---
+++ client/src/otherPage.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/"></Link>
+    </div>
+  );
+};
